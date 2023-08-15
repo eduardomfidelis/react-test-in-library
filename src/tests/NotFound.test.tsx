13662eaaa561b003a15cd0a0be09d17a34b1cb16
eduardo maciel <eduardomacielfidelis@gmail.com>
2023-08-15 19:53:40 -0300

    requisito(4)Testa a página NotFound

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,13 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+test('Teste o componente not found', () => {
+  render(<NotFound />);
+  expect(screen.getByRole('heading', { name: 'Page requested not found' }));
+});
+test('teste a imagem no componente notFound', () => {
+  render(<NotFound />);
+  const image = screen.getByAltText("Clefairy pushing buttons randomly with text I have no idea what i'm doing");
+  expect(image).toBeInTheDocument();
+  expect(image).not.toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
