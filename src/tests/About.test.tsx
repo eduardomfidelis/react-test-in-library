f2f7dd74ec217713bb407b7d4b7f9bc9e9abe059
eduardo maciel <eduardomacielfidelis@gmail.com>
2023-08-15 16:00:35 -0300

    requisito(2) teta o componente about

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,18 +1,25 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-
 import { About } from '../pages';
 
-test('Teste se a página contém as informações sobre a Pokédex.', () => {
-  render(<About />, { wrapper: BrowserRouter });
+describe('Testa o componente about', () => {
+  it('Deve possuir h2 com o texto "About Pokédex"', () => {
+    render(<About />);
+    const heading = screen.getByText(/About Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-  expect(screen.getByRole('heading', { name: 'About Pokédex' }));
-  const parag = screen.getAllByRole('p');
-  expect(parag).toHaveLength(2);
+  test('Deve ter dois paragrafos sobre pokédex', () => {
+    render(<About />);
+    expect(screen.getByText(/This application simulates a Pokédex/i));
+    expect(screen.getByText(/One can filter Pokémon by type/i));
+  });
 
-  const altText = 'Pokédex';
-  const imageUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
-  const imageElement = screen.getByAltText(altText);
-
-  expect(imageElement.getAttribute('src')).toBe(imageUrl);
+  test('testa a imagem da pokedex', () => {
+    render(<About />);
+    const image = screen.getByAltText('Pokédex');
+    expect(image).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
 });
