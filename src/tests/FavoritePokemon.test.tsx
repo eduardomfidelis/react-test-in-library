0aee0f54ac013b914519ffbaca787e369c6a6ffc
eduardo maciel <eduardomacielfidelis@gmail.com>
2023-08-18 01:36:29 -0300

    requisito(2)Teste o componente <FavoritePokemon.tsx />

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,23 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+import App from '../App';
+
+test('Teste o componente <FavoritePokemon.tsx />', async () => {
+  renderWithRouter(<FavoritePokemon />);
+  const notFoundMenssage = screen.getByText(/no favorite pokémon found/i);
+  expect(notFoundMenssage).toBeInTheDocument();
+});
+test('Teste se são exibindo apenas os Pokémons favoritados ', async () => {
+  renderWithRouter(<App />);
+  const moreDetails = screen.getByRole('link', { name: 'More details' });
+  await userEvent.click(moreDetails);
+  const checkbox = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+  await userEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+
+  const favoritePokemon = screen.getByRole('link', { name: /Favorite Pokémon/i });
+  await userEvent.click(favoritePokemon);
+  screen.getByText('Pikachu');
+});
--- src/tests/Pokemon.test.tsx
+++ src/tests/Pokemon.test.tsx
@@ -1,9 +1,10 @@
-/* import { render, screen } from '@testing-library/react';
-
-test('', () => {});
+/*
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
 
 test('Teste o componente <Pokemon.tsx />', () => {
-  render(<App />);
+  render(<App />, { wrapper: BrowserRouter });
   screen.getByText('Pikachu');
   screen.getByText('Electric');
   screen.getByText('Average weight: 6.0 kg');
